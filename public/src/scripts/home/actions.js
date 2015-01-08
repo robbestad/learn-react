198e23946d96222cf270dc4afadba989f86c413b
Sven Anders Robbestad <robbestad@gmail.com>
2015-01-08 12:17:16 +0100

    added home actions and store

---
+++ public/src/scripts/home/actions.js
@@ -0,0 +1,19 @@
+var Reflux = require('reflux');
+
+// Create a Reflux actions object with basic actions for reloading posts
+// and deleting a post
+var actions = Reflux.createActions([
+    'deletePost',
+    'reloadPosts'
+]);
+
+// Force deletePosts to take a single argument
+actions.deletePost.shouldEmit = function(postId){
+    var isValidId = typeof postId === 'number';
+    if(!isValidId)
+        console.error('reflux-example:deletePost action must be called with a number for the post ID to be deleted.');
+    return isValidId;
+};
+
+// Export the actions
+module.exports = actions;
---
+++ public/src/scripts/home/store.js
@@ -0,0 +1,77 @@
+var Reflux = require('reflux'),
+    actions = require('./actions'),
+    $ = require("jquery");
+
+// Some constants for determining state; will be exported on store too
+var STATE_LOADING = 'loading',
+    STATE_OK = 'ok',
+    STATE_ERR = 'err';
+
+// Create a private variable to store the store's state
+var store = {
+    state: STATE_LOADING
+};
+
+// Export a new Reflux store
+module.exports = Reflux.createStore({
+    // Constants for marking state of store
+    STATE_LOADING: STATE_LOADING,
+    STATE_OK: STATE_OK,
+    STATE_ERR: STATE_ERR,
+
+    // Hook up the store to the actions in `actions.js`
+    listenables: actions,
+
+    // Add some getters
+    getData: function(){
+        return store;
+    },
+
+    // Pull posts from server and trigger to let listeners know
+    // posts changed
+    updateFromServer: function(){
+        $.get('/assets/posts.json')
+            .done(function(data){
+                store = {
+                    state: STATE_OK,
+                    posts: data
+                };
+            }.bind(this))
+            .fail(function(err){
+                store.state = STATE_ERR;
+                store.error = 'Could not fetch posts from server';
+                if(err.responseJSON && err.responseJSON.error){
+                    store.error += ': ' + err.responseJSON.error;
+                }else{
+                    store.error += '.';
+                }
+            }.bind(this))
+            .always(function(){
+                this.trigger(store);
+            }.bind(this));
+    },
+
+    // Simply reload posts on init
+    init: function(){
+        this.reloadPosts();
+    },
+
+    // Delete a post based on ID.
+    // TODO Handle ID not found; currently does nothing
+    deletePost: function(postId){
+        store.posts = store.posts.filter(function(post){
+            return post.id !== postId;
+        });
+        this.trigger(store);
+    },
+    // Set default data, put into loading state, kick off posts fetch from
+    // server, and let listeners know
+    reloadPosts: function(){
+        store = {
+            state: STATE_LOADING,
+            posts: []
+        };
+        this.trigger(store);
+        this.updateFromServer();
+    }
+});
