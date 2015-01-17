a77525483c9e68e8c9b92932f73197c13b7746df
Sven Anders Robbestad <robbestad@gmail.com>
2015-01-17 11:03:12 +0100

    New style

--- public/src/scripts/home/index.js
+++ public/src/scripts/home/index.js
@@ -27,24 +27,29 @@ module.exports = React.createClass({
         this.setState({editable: !this.state.editable});
     },
     render():any {
-        return <div>
+        return <div >
             <Breadcrumbs />
-            <h1>Home</h1>
+            <div className="flyin-widget">
+                <h1>Home</h1>
 
             <p>
             Welcome to my collection of React examples, modules and tutorials. More will be coming
                 every now and then, so be sure to bookmark and come back.
             </p>
 
-            <h3>Editable test</h3>
-            <input type="button" value="Edit" onClick={this.onClick} />
-            <Editable editable={this.state.editable}
-            html="<b>H</b>el<b>lo </b><i><b>W</b>or<b>l</b>d<b>!</b></i>"
-            ref="editable" onChange={this.onChange} />
+
+        </div>
         </div>
     }
 
 });
+
+//
+//<h3 >Editable test</h3>
+//<input type="button" value="Edit" onClick={this.onClick} />
+//<Editable editable={this.state.editable}
+//    html="<b>H</b>el<b>lo </b><i><b>W</b>or<b>l</b>d<b>!</b></i>"
+//    ref="editable" onChange={this.onChange} />
 
 //<Editable editable={this.state.editable}
 //    html="<b>H</b>el<b>lo </b><i><b>W</b>or<b>l</b>d<b>!</b></i>"
