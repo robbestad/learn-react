5699b195a472f11113629d82f2584357ecb62772
Sven Anders Robbestad <robbestad@gmail.com>
2015-03-13 20:38:06 +0100

    timeout added

--- public/src/scripts/login/index.js
+++ public/src/scripts/login/index.js
@@ -40,23 +40,24 @@ const Instructions = React.createClass({
                     React, but the principle applies no matter what kind of
                     JavaScript library you're using.
                 </p>
-                    <p>
-                        One thing to note is that the
-                     API is configured to authenticate with Oauth2.
-                    </p>
-                    <blockquote>
-                        OAuth 2.0 is the next evolution of the OAuth protocol which was
-                        originally created in late 2006. OAuth 2.0 focuses on client
-                        developer simplicity while providing specific authorization flows
-                        for web applications, desktop applications, mobile phones,
-                        and living room devices. Source: <a href="http://oauth.net/2/" target="_blank">ouath.net</a>
-                    </blockquote>
+                <p>
+                    One thing to note is that the
+                    API is configured to authenticate with Oauth2.
+                </p>
+                <blockquote>
+                    OAuth 2.0 is the next evolution of the OAuth protocol which was
+                    originally created in late 2006. OAuth 2.0 focuses on client
+                    developer simplicity while providing specific authorization flows
+                    for web applications, desktop applications, mobile phones,
+                    and living room devices. Source: <a href="http://oauth.net/2/" target="_blank">ouath.net</a>
+                </blockquote>
                 <p>
                     In a regular server-based application I would have requested
                     access with a combination of the users login information coupled
                     with a secret client key, and then been given an authorization code
                     which I would have stored securely in a session.
-                </p><p>
+                </p>
+                <p>
                     In a browser-based application, this method is not secure, so instead
                     I'm going to use something that is known as an <em>implicit grant</em> to
                     request access.
@@ -67,7 +68,6 @@ const Instructions = React.createClass({
                     than an authorization code being returned from the
                     authorization request, a token is returned from the API.
                 </p>
-
                 <blockquote>
                     The implicit grant type is used to obtain access tokens and is
                     optimized for public clients known to operate a particular
@@ -79,20 +79,20 @@ const Instructions = React.createClass({
                     an access token, the client receives the access token as
                     the result of the authorization request.
                     </blockquote>
-
-
-
-                <p>Here's what happens. When you click <em>Login</em>, a POST request is sent to a third-party service
-                    located at https://morning-forest-9780.herokuapp.com/.
-                    </p>
                 <p>
-                     If the credentials are approved, the API
+                    Here's what happens. When you click <em>Login</em>, a POST request is sent to a third-party service
+                    located at https://morning-forest-9780.herokuapp.com/.
+                </p>
+                <p>
+                    If the credentials are approved, the API
                     returns a couple of tokens in its response (an <em>access_token</em> and a <em>refresh_token</em>).
                 </p>
-                <p>The use of tokens are important from a security point of view, because they contain
+                <p>
+                    The use of tokens are important from a security point of view, because they contain
                     no login credentials and are only valid for a limited time.
                 </p>
-                <p>When you receive these tokens, the flux store sets a <em>loggedIn</em> variable to
+                <p>
+                    When you receive these tokens, the flux store sets a <em>loggedIn</em> variable to
                     true, which means that you have an easy way of knowing whether your user is authenticated or not.
                     In this example, a call to the function <strong>LoginStore.isAuthenticated()</strong> is used
                     to display login information if the user is logged in.
