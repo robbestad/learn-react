db3b87c183abab17ebd3b8bc64bca3ac9e864e41
Sven Anders Robbestad <robbestad@gmail.com>
2015-03-19 13:32:49 +0100

    renamed jsx to js

---
+++ public/src/scripts/articles/item/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Breadcrumbs from 'react-breadcrumbs';
+import {Bootstrap, Grid, Col, Row, Button} from 'react-bootstrap';
+import Router,{Link} from 'react-router';
+
+export default React.createClass({
+    mixins:[Router.Navigation, Router.State],
+    displayName:"Article",
+    render() {
+        return <Grid className="flyin-widget">
+            <Row className="show-grid">
+                <Col md={12}>
+                    <Breadcrumbs breadcrumbName={this.getParams().id} />
+                </Col>
+            </Row>
+            <Row className="show-grid">
+                <Col md={12}>
+
+                    The article
+                </Col>
+            </Row>
+        </Grid>
+    }
+});
