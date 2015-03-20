e319ec6e0053189cd5ed0750e13bfe159525f95c
Sven Anders Robbestad <robbestad@gmail.com>
2015-03-20 08:28:11 +0100

    Updated render

--- public/src/scripts/forms/index.js
+++ public/src/scripts/forms/index.js
@@ -39,7 +39,7 @@ module.exports = React.createClass({
         });
     },
 
-    render: function () {
+    render: () => {
         return <Grid className="flyin-widget">
             <Row className="show-grid">
                 <Col md={12}><Breadcrumbs /></Col>
