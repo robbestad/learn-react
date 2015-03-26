c6d1ba75f0714c090eaf099ed67ec986fc24b17e
Sven Anders Robbestad <robbestad@gmail.com>
2015-03-26 10:09:20 +0100

    Updated stickydiv

--- public/src/scripts/stickydiv/index.js
+++ public/src/scripts/stickydiv/index.js
@@ -15,16 +15,23 @@ module.exports = React.createClass({
     displayName: "Sticky Div",
     render():any {
         return (<span className="flyin-widget">
-                <Sticky offsetTop={60} zIndex={10}>
-                    <span className="centered" style={{
-                        backgroundColor: '#fff', color: 'red',
-                        fontSize: '24px', padding: '2px', opacity:1
-                    }}>I'm really sticky</span>
-                </Sticky>
-                <Grid >
-                    <Row className="show-grid">
-                        <Col md={12}>
+            <Sticky offsetTop={70} zIndex={10}>
+                <span className="centered" style={{
+                    backgroundColor: '#fff', color: 'red',
+                    fontSize: 24, padding: 2, opacity: 1,
+                    position:'absolute',top:-73, width:'100%'
+                }}>I'm really sticky</span>
+            </Sticky>
+            <Grid >
+                <Row className="show-grid">
+                    <Col md={12}>
                         <Breadcrumbs />
+
+                    </Col>
+                </Row>
+
+                <Row className="show-grid">
+                    <Col md={12}>
                         <p>Sample text to fill up the page</p>
                         <div>Ab adipisci, aliquid beatae consectetur dolore enim expedita iste iusto nesciunt officia optio pariatur porro quam qui quibusdam quidem quod reiciendis tenetur. Eveniet ipsa provident quas quia quis ratione voluptatibus.</div>
                         <div>Nisi non sint veritatis. Dignissimos doloremque expedita minus odio quasi quibusdam rerum! Aperiam eum ex, fugit incidunt molestias nihil placeat porro sapiente temporibus ut. Doloremque expedita laborum mollitia optio vel.</div>
@@ -40,12 +47,11 @@ module.exports = React.createClass({
                         <div>Nisi non sint veritatis. Dignissimos doloremque expedita minus odio quasi quibusdam rerum! Aperiam eum ex, fugit incidunt molestias nihil placeat porro sapiente temporibus ut. Doloremque expedita laborum mollitia optio vel.</div>
                         <div>Animi aut, ipsum mollitia possimus quas sed! Commodi cum delectus ducimus eos eum harum laborum praesentium quasi ratione saepe! Accusantium atque cupiditate dolores fugiat id ipsam laboriosam molestiae nulla similique.</div>
                         <div>Dolore dolorem enim illum incidunt non. Deserunt dolor hic, laudantium neque nesciunt nihil nulla porro! Dignissimos fugit porro praesentium rem voluptatum! Aliquam debitis deserunt eius esse expedita voluptatum. Possimus, tenetur!</div>
-                            <Breadcrumbs />
-                        </Col>
-                    </Row>
+                    </Col>
+                </Row>
 
             </Grid>
-            </span>
+        </span>
 
         )
 
