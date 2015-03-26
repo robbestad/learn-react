996c2ecd9b10dbd26d28c115ced6e0fe7555f2c1
Sven Anders Robbestad <robbestad@gmail.com>
2015-03-26 09:48:28 +0100

    Updated stickydiv

--- public/src/scripts/stickydiv/index.js
+++ public/src/scripts/stickydiv/index.js
@@ -7,30 +7,25 @@
 
 const React = require('react');
 
-var Sticky = require('../react-stickydiv');
+var Sticky = require('react-stickydiv');
 const Breadcrumbs = require('react-breadcrumbs'),
     {Grid, Col, Row} = require('react-bootstrap');
 
 module.exports = React.createClass({
     displayName: "Sticky Div",
     render():any {
-        return <Grid className="flyin-widget">
-
-                <Row className="show-grid">
-                    <Col md={12}><Breadcrumbs /></Col>
-                </Row>
-
-                <Row className="show-grid">
-                    <Col md={12}>
-                        <Sticky >
-                            <span className="centered" style={{
-                                backgroundColor: 'transparent', color: 'red',
-                                fontSize: '24px', padding: '2px'
-                            }}>I'm really sticky</span>
-                        </Sticky>
-
+        return (<span className="flyin-widget">
+                <Sticky offsetTop={60} zIndex={10}>
+                    <span className="centered" style={{
+                        backgroundColor: '#fff', color: 'red',
+                        fontSize: '24px', padding: '2px', opacity:1
+                    }}>I'm really sticky</span>
+                </Sticky>
+                <Grid >
+                    <Row className="show-grid">
+                        <Col md={12}>
+                        <Breadcrumbs />
                         <p>Sample text to fill up the page</p>
-                        <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad adipisci distinctio doloribus earum fugit id incidunt iste itaque minus modi nisi nulla omnis perspiciatis placeat provident tenetur ullam unde, voluptatum.</div>
                         <div>Ab adipisci, aliquid beatae consectetur dolore enim expedita iste iusto nesciunt officia optio pariatur porro quam qui quibusdam quidem quod reiciendis tenetur. Eveniet ipsa provident quas quia quis ratione voluptatibus.</div>
                         <div>Nisi non sint veritatis. Dignissimos doloremque expedita minus odio quasi quibusdam rerum! Aperiam eum ex, fugit incidunt molestias nihil placeat porro sapiente temporibus ut. Doloremque expedita laborum mollitia optio vel.</div>
                         <div>Animi aut, ipsum mollitia possimus quas sed! Commodi cum delectus ducimus eos eum harum laborum praesentium quasi ratione saepe! Accusantium atque cupiditate dolores fugiat id ipsam laboriosam molestiae nulla similique.</div>
@@ -45,11 +40,14 @@ module.exports = React.createClass({
                         <div>Nisi non sint veritatis. Dignissimos doloremque expedita minus odio quasi quibusdam rerum! Aperiam eum ex, fugit incidunt molestias nihil placeat porro sapiente temporibus ut. Doloremque expedita laborum mollitia optio vel.</div>
                         <div>Animi aut, ipsum mollitia possimus quas sed! Commodi cum delectus ducimus eos eum harum laborum praesentium quasi ratione saepe! Accusantium atque cupiditate dolores fugiat id ipsam laboriosam molestiae nulla similique.</div>
                         <div>Dolore dolorem enim illum incidunt non. Deserunt dolor hic, laudantium neque nesciunt nihil nulla porro! Dignissimos fugit porro praesentium rem voluptatum! Aliquam debitis deserunt eius esse expedita voluptatum. Possimus, tenetur!</div>
+                            <Breadcrumbs />
+                        </Col>
+                    </Row>
 
-                    </Col>
+            </Grid>
+            </span>
 
-                </Row>
+        )
 
-            </Grid>;
     }
 });
