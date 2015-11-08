fee99cd4bea9a3fd5fb84c2bad4b2124fcfff2dd
Sven A Robbestad <robbestad@gmail.com>
2015-11-08 21:23:16 +0100

    Updated to latest repos

--- dev.js
+++ dev.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config.dev');
+var config = require('./webpack.config');
 
 new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,65 @@
-var webpack = require('webpack');
+var
+  webpack = require('webpack'),
+  path = require('path'),
+  nodeRoot = path.resolve(__dirname, "node_modules");
 
 var config = {
-    entry: "./public/src/scripts/app.js",
-    output: {
-        filename: "public/dist/bundle.js"
-    },
-    module: {
-        loaders: [
-            { test: /\.js?$/, loaders: ['jsx?harmony','babel-loader'], exclude: /node_modules/ }
-        ]
-    },
-    plugins: [
-    ],
-
-    resolve: {
-      // you can now require('file') instead of require('file.js')
-      extensions: ['', '.js', '.json', '.jsx'],
-      modulesDirectories: ['node_modules', 'vendors', 'public/src'],
-      alias: {
-        app: '/public/src/scripts'
+  entry: "./public/src/scripts/app.js",
+  output: {
+    filename: "public/dist/bundle.js"
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: nodeRoot,
+        include: path.resolve(__dirname, "public/src/scripts")
       }
+    ]
+  },
+  plugins: [],
+  devtool: "#eval",
+  // don't parse some dependencies to speed up build.
+  noParse: [
+    nodeRoot,
+    path.resolve(__dirname, 'public/src/assets/'),
+    path.resolve(__dirname, 'public/src/components/'),
+    path.resolve(__dirname, 'public/src/img/'),
+    path.resolve(__dirname, 'public/src/lib/'),
+    path.resolve(__dirname, 'public/src/styles/'),
+    path.resolve(__dirname, 'public/src/*.map')
+  ],
+  cache: true,
+  resolve: {
+    // you can now require('file') instead of require('file.js')
+    extensions: ['', '.js', '.json', '.jsx'],
+    modulesDirectories: ['node_modules', 'vendors', 'public/src'],
+    alias: {
+      app: '/public/src/scripts'
     }
+  }
 };
+
+if (process.env.NODE_ENV === 'development') {
+// Development Config
+  config.entry = ['webpack/hot/dev-server', 'webpack-dev-server/client?http://localhost:8080', './public/src/scripts/app.js'];
+  config.devtool = 'eval';
+  config.output = {
+    path: __dirname + '/build',
+    filename: 'bundle.js'
+  };
+  config.module = {
+    loaders: [
+      {test: /[\.js?$|\.jsx?$]/, loaders: ['react-hot', 'jsx?harmony', 'babel-loader'], exclude: /node_modules/}
+    ]
+  };
+  config.resolve = {
+    extensions: ['', '.js', '.jsx']
+  };
+  config.plugins = [
+    new webpack.NoErrorsPlugin()
+  ]
+}
 
 module.exports = config;
